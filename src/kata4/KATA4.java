7e5ad597ef50dbed672e5f104dd5c92662e20206
usuario <usuario@10.22.143.10>
2015-10-29 10:16:29 +0000

    version5

--- src/kata4/KATA4.java
+++ src/kata4/KATA4.java
@@ -1,61 +1,29 @@
 package kata4;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.Date;
+import java.util.ArrayList;
 
 public class KATA4 {
 
-    public static void main(String[] args) throws FileNotFoundException, IOException {
+    public static void main(String[] args) throws IOException {
 
-        String pathname = "c:\\Users\\usuario\\Documents\\";
-        File file = new File(pathname);
-        String [] dire = file.list();
+        String nameFile = "C:\\Users\\usuario\\Documents\\NetBeansProjects\\KATA4\\DATA\\emailsfilev1.txt";
+        ArrayList<String> mailArray = MailListReader.read(nameFile);
+        System.out.println(mailArray.size());
+
+        Histogram<String> histogram = new Histogram<>();
+        histogram.increment("gmail");
+        histogram.increment("gmail");
+        histogram.increment("gmail");
+
+        histogram.increment("ulpgc");
+        histogram.increment("ulpgc");
+        histogram.increment("ulpgc");
+        histogram.increment("ulpgc");
+        histogram.increment("ulpgc");
         
-        /*for (String dire1 : dire) {
-            System.out.println(dire1);
-        }*/
+        new HistogramDisplay(histogram).execute();
         
-        String from = "C:\\Users\\Public\\Pictures\\Sample Pictures\\Desert.jpg";
-        String to = "C:\\Users\\Public\\Pictures\\Sample Pictures\\Desert(1).jpg";
-        BufferedInputStream input = new BufferedInputStream(new FileInputStream(new File(from)));
-        BufferedOutputStream output = new BufferedOutputStream(new FileOutputStream(new File(to)));
-        
-        byte [] buffer = new byte[1024];
-        
-        Date date = new Date();
-        
-        while(true){
-            int read = input.read(buffer);
-            if (read < 0) break;
-            output.write(buffer); 
-        }
-        System.out.println(new Date().getTime()-date.getTime()+"msg");
-        
-        input.close();
-        output.flush();
-        output.close();
-        
-        //print(file.listFiles(),"");
     }
 
-    private static void print(File[] listFiles, String string) {
-        if (listFiles == null) {
-            return;
-        }
-        
-        for (File listFile : listFiles) {
-            System.out.println(string+(listFile.isDirectory()?"+":"->")+listFile.getName());
-            if (listFile.isFile() || listFile.isHidden()) continue;
-            print(listFile.listFiles(), string + " ");
-            
-        }
-    }
-    
 }
