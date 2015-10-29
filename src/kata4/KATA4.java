6edeaa879a9da62f6a00896c1f251753fc0c3b1b
usuario <usuario@10.22.143.10>
2015-10-29 09:18:59 +0000

    version3-copia un fichero

--- src/kata4/KATA4.java
+++ src/kata4/KATA4.java
@@ -1,20 +1,46 @@
 package kata4;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Date;
 
 public class KATA4 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
 
         String pathname = "c:\\Users\\usuario\\Documents\\";
         File file = new File(pathname);
         String [] dire = file.list();
         
-        for (String dire1 : dire) {
+        /*for (String dire1 : dire) {
             System.out.println(dire1);
-        }
+        }*/
         
-        print(file.listFiles(),"");
+        String from = "C:\\Users\\Public\\Pictures\\Sample Pictures\\Desert.jpg";
+        String to = "C:\\Users\\Public\\Pictures\\Sample Pictures\\Desert(1).jpg";
+        BufferedInputStream input = new BufferedInputStream(new FileInputStream(new File(from)));
+        BufferedOutputStream output = new BufferedOutputStream(new FileOutputStream(new File(to)));
+        
+        Date date = new Date();
+        
+        while(true){
+            int read = input.read();
+            if (read < 0) break;
+            output.write(read); 
+        }
+        System.out.println(new Date().getTime()-date.getTime()+"msg");
+        
+        input.close();
+        output.flush();
+        output.close();
+        
+        //print(file.listFiles(),"");
     }
 
     private static void print(File[] listFiles, String string) {
